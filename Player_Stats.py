9ae69bfa29b214ecee0e8c0b7d83fe5640a42564
ThanReserMo <86783250+ThanReserMo@users.noreply.github.com>
2026-06-29 00:21:12 +0300

    Update Player_Stats.py

--- Player_Stats.py
+++ Player_Stats.py
@@ -17,7 +17,7 @@ st.title('Hi! This is a personal data analysis and engineering project about sta
 
 st.write('The data set used for this project comes from the Kaggle link below!')
 
-st.link_button(label='Kaggle link','https://www.kaggle.com/datasets/zq1200/world-chess-championships-1866-to-2021')
+st.link_button(label='Kaggle link',url='https://www.kaggle.com/datasets/zq1200/world-chess-championships-1866-to-2021')
 
 st.write('Use the buttons in the sidebar to navigate the site!')
 
