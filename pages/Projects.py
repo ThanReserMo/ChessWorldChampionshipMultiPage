b74d6f8b51c19620b1194a49ec330fb633f6e170
ThanReserMo <86783250+ThanReserMo@users.noreply.github.com>
2026-06-29 10:06:06 +0000

    Added some text

--- pages/Projects.py
+++ pages/Projects.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title('Hi!')
+st.header('What information can we find out from the given dataset?')
+
+st.write('We can take a player oriented approach, finding the most frequent openings as well as the results of the games of a specific player within a certain time range!')
+
+st.write('Taking an opening-first approach, we can track the popularity of openings throughout the tournament\'s history!')
