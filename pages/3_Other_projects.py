480e518044dfccf4f7ffa96a46a7f87f2af8c7a4
ThanReserMo <86783250+ThanReserMo@users.noreply.github.com>
2026-06-30 11:35:11 +0000

    Added another page

---
+++ pages/3_Other_projects.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header('Check out my other projects!')
+
+url = "https://spotifystats2023.streamlit.app/"
+
+st.markdown("Check out my brief analysis of the most popular songs of 2023 [here](%s)! " % url)
